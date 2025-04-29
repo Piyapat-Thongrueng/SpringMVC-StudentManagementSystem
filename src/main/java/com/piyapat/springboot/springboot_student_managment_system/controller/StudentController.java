0453973fa3ff43e5e9a1b9742f30612c0ae3c1d9
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-29 18:33:30 +0700

    configure DELETE and VIEW student feature

--- src/main/java/com/piyapat/springboot/springboot_student_managment_system/controller/StudentController.java
+++ src/main/java/com/piyapat/springboot/springboot_student_managment_system/controller/StudentController.java
@@ -77,7 +77,19 @@ public class StudentController {
         return "redirect:/students";
     }
 
+    // Handler method to handle delete student request
+    @GetMapping("/students/{studentId}/delete")
+    public String deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+        return "redirect:/students";
+    }
 
-
-
+    // Handler method to handle view student request
+    @GetMapping("/students/{studentId}/view")
+    public String viewStudent(@PathVariable("studentId") Long studentId,
+                              Model model){
+        StudentDto studentDto = studentService.getStudentById(studentId);
+        model.addAttribute("student", studentDto);
+        return "view_student";
+    }
 }
