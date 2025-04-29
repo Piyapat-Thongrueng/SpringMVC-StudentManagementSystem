350715190c8a1c66f185e4e753e6cdae165f7d36
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-29 10:14:35 +0700

    Create project and make MySQL Database connection

---
+++ src/test/java/com/piyapat/springboot/springboot_student_managment_system/SpringbootStudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyapat.springboot.springboot_student_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
